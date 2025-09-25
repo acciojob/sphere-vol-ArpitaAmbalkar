f8b2cbfb7d8066ab47d0ed5a9a73774d0fe71562
ArpitaAmbalkar <arpitaambalkar26@gmail.com>
2025-09-25 18:36:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
 function volume_sphere() {
     //Write your code here
-  
+	 event.preventDefault();
+    let r = document.getElementById("radius").value;
+    r = parseFloat(r); // allow decimals too
+
+    // calculate volume
+	let vol = document.getElementById("volume");
+	if(r<0 || isNaN(r))
+	{
+		vol.value=NaN;
+		return;
+	}
+    vol = (4/3) * Math.PI * Math.pow(r, 3);
+
+    // show result
+    vol.value = vol.toFixed(4);
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById("MyForm").onsubmit = volume_sphere;
+}
